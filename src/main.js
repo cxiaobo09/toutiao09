bcdbedf941a94d3620315fe9cb0e73fcd0e96c70
cxiaobo09 <cxiaobo1996@163.com>
2020-02-29 14:56:23 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElmentUI from 'elment-ui'
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+Vue.use(ElmentUI)
 Vue.config.productionTip = false
 
 new Vue({
