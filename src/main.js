013db486019f96ab3f21fcf799fa1e31366cc72c
cxiaobo09 <cxiaobo1996@163.com>
2020-02-29 19:59:24 +0800

    登陆界面静态图形

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 引入elementui组件
 import ElmentUI from 'element-ui'
 import '@/styles/index.less'
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
-Vue.use(ElmentUI)
+Vue.use(ElmentUI) // 全局定义
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,15 +1,64 @@
 <template>
-<div>
-    <h1>登录页面哦10</h1>
-</div>
+  <div class="login">
+    <!-- 表单 -->
+    <el-card class="form-card" style="opacity:0.6">
+      <!-- logo图标 -->
+      <div class="logo">
+        <img src="../../assets/logo_index.png" alt />
+      </div>
+      <!-- 输入表单 用element组件-->
+      <el-form  style="margin-top:20px;">
+          <!-- 手机号 -->
+        <el-form-item>
+          <el-input placeholder="输入手机号"></el-input>
+        </el-form-item>
+        <!-- 验证码 -->
+        <el-form-item>
+          <el-input placeholder="输入验证码" style="width:60%;"></el-input>
+          <el-button plain style="float:right;">验证码</el-button>
+        </el-form-item>
+        <!-- 勾选栏 -->
+        <el-form-item>
+          <el-checkbox label="我已同意----"></el-checkbox>
+        </el-form-item>
+        <!-- 按钮 -->
+        <el-form-item>
+          <el-button style="width: 100%;" type="primary" @click="onSubmit">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {}
+  },
+  methods: {
+    onSubmit () {}
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.login {
+  background-image: url("../../assets/BGBymine.jpg");
+  height: 100vh;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .form-card {
+    width: 400px;
+    height: 320px;
+    background-color: rgb(86, 38, 141);
+    .logo {
+      text-align: center;
+      img {
+        height: 40px;
+      }
+    }
+  }
+}
 </style>
